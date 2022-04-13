4a74e0052cf38969639b1708014fc00ddc598dfb
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-13 10:26:45 +0200

    Updated to React 18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</React.StrictMode>);
+const container = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
