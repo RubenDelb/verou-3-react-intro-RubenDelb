cdc7dfd761fc4b671924178fa206f7c1a52d12bc
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-13 10:57:58 +0200

    Set strictmode on the whole application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
+import React from 'react';
 
 const container = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(container);
-root.render(<App />);
+root.render(<React.StrictMode> <App /> </React.StrictMode> );
