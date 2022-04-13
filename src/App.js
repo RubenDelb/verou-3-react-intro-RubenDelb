ef9a86a1c8790f9b925be93053a632a954d0ff90
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-13 10:47:56 +0200

    Import and use all the created components and send hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid';
+import Header from './Header';
+import Input from './Input';
+import Button from './Button';
 // import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className='grid justify-center text-center'>
+      <Header />
+      <Input placeHolder={"Enter todo here..."}/>
+      <Button btnName={"Add"} />
+      <Button btnName={"Remove completed todos"} />
+      <TodoList todoName={"testtt"}/>
+    </div>
   );
 }
 
