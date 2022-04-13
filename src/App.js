36eae7e1000d837cf273bfb1f2b58b9f9cb8597e
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-13 11:49:36 +0200

    Send functions and values to the components as hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import TodoList from "./TodoList";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 } from 'uuid';
 import Header from './Header';
 import Input from './Input';
 import Button from './Button';
 // import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  
+  // create a todolist with useState 
+  const [todos, setTodos] = useState([])
+  // catch any input that's given with useRef
+  const inputRef = useRef()
+
+  // Set new todo in the "todos"-array with the value of what's been inputted in the inputfield 
+  const addTodo = () => {
+    const inputName = inputRef.current.value // catch inputvalue
+    if (inputName === "") return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: v4(), name: inputName, complete: false}]
+    })
+    // Make the inputfield empty again after adding the todo
+    inputRef.current.value = ""
+  }
 
   return (
     <div className='grid justify-center text-center'>
-      <Header />
-      <Input placeHolder={"Enter todo here..."}/>
-      <Button btnName={"Add"} />
+      <Header headerTitle={"My Todo-List"} />
+      <Input placeHolder={"Enter todo here..."} inputValue={inputRef} />
+      <Button btnName={"Add"} btnFunction={addTodo}/>
       <Button btnName={"Remove completed todos"} />
-      <TodoList todoName={"testtt"}/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
