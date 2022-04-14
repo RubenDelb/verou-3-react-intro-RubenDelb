5067eb6b1f9345345153c45675bd982349ddec84
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-14 09:20:05 +0200

    Remove strictmode because it breaks "setTodos from localstorage"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
-import React from 'react';
 
 const container = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(container);
-root.render(<React.StrictMode> <App /> </React.StrictMode> );
+root.render(<App />);
