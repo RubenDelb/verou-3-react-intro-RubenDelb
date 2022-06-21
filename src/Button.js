a71f7e7e714faf94822932660c93ce1797943f8c
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-21 15:34:37 +0200

    Add button shadow on hover

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 const Button = ({ btnName, btnFunction }) => {
     return (
-        <button className="border-2 bg-gray-100 rounded m-1 p-1" onClick={btnFunction}>{btnName}</button>
+        <button className="border-2 bg-gray-100 rounded m-1 p-1 hover:shadow-md" onClick={btnFunction}>{btnName}</button>
     )
 }
 
